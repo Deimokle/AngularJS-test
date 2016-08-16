03e4f65bc466c54060f82fe0097d14a91d1b5a0a
Deimokle <q.dutrevis@gmail.com>
2016-08-16 11:50:33 +0200

    bug

--- public/js/controllers/maincontroller.js
+++ public/js/controllers/maincontroller.js
@@ -2,12 +2,12 @@ function mainController($scope, todoService){
     $scope.nom="";
     $scope.vie="";
     $scope.date="";
-    $scope.cat=[Feu, Eau, Roche];
+    $scope.cat=["Feu", "Eau", "Roche"];
     $scope.pc="";
     $scope.exp="";
 
     $scope.create = function(){
-        todoService.create({name:$scope.nom, vie:$scope.vie, date:$scope.date, cat:$scope.cat, pc:$scope.pc, exp:$scope.exp})
+        todoService.create({nom:$scope.nom, vie:$scope.vie, date:$scope.date, cat:$scope.cat, pc:$scope.pc, exp:$scope.exp})
         .then(function(){
             $scope.getAll();
         })
@@ -22,7 +22,7 @@ function mainController($scope, todoService){
     }
 
     $scope.update = function(id,data){
-        todoService.update(id, {name:data.nom, vie:data.vie, date:data.date, cat:data.cat, pc:data.pc, exp:data.exp})
+        todoService.update(id, {nom:data.nom, vie:data.vie, date:data.date, cat:data.cat, pc:data.pc, exp:data.exp})
         .then(function(res){
             $scope.getAll();
         })
